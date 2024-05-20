5f3e622f73096cfc9c524c874e8257239be2d303
yuliia_sharapova <juliaa.sharapovaa@gmail.com>
2024-05-20 17:35:23 +0300

    resolve tasks

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name - input(подія input)
+// підставляє його поточне значення в span#name - output як ім’я для привітання.Обов’язково
+// очищай значення в інпуті по краях від пробілів.Якщо інпут порожній або містить лише
+// пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+document.addEventListener('DOMContentLoaded', () => {
+  const textInput = document.querySelector('input#name-input');
+  const output = document.querySelector('span#name-output');
+  textInput.addEventListener('input', event => {
+    const trimmedText = event.currentTarget.value.trim();
+    output.textContent = trimmedText === '' ? 'Anonymus' : trimmedText;
+  });
+});
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,26 @@
+// Напиши скрипт, який змінює колір фону елемента < body > через інлайн - стиль по кліку на
+// button.change - color і задає це значення кольору текстовим вмістом для span.color.
+//<div class="widget">
+// <p>
+//  Background color: <span class="color">-</span>
+// </p>
+// <button type="button" class="change-color">
+//    Change color
+//  </button>
+//</div>;
+// Зверни увагу, що функція getRandomHexColor() повертає колір у hex - форматі, в той час як
+// колір фону на < body > буде у форматі rgb.Це нормально й не потребує якихось правок.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('button.change-color');
+const output = document.querySelector('span.color');
+button.addEventListener('click', event => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  output.textContent = newColor;
+});
